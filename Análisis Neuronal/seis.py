6793715edc4c101de8317f3294223efc3de1d023
iarobotixyz <62524097+iarobotixyz@users.noreply.github.com>
2020-10-27 05:35:18 -0600

    Add files via upload

---
+++ Análisis Neuronal/seis.py
@@ -0,0 +1,23 @@
+import cv2
+gray = cv2.imread('neurona.tif', cv2.IMREAD_GRAYSCALE)#abre imagen
+t, dst = cv2.threshold(gray, 170, 255, cv2.THRESH_BINARY)#convierte imagen a binario
+t, dsti = cv2.threshold(gray, 170, 255, cv2.THRESH_BINARY_INV)#convierte imagen a binario invertido
+t, dti = cv2.threshold(gray, 170, 255, cv2.THRESH_TRUNC)#convierte imagen a diferencia de pixel
+t, dto = cv2.threshold(gray, 170, 255, cv2.THRESH_TOZERO)#Cualquier pixel cuya intensidad no supere el umbral se establecerá a cero
+t, dtoi = cv2.threshold(gray, 170, 255, cv2.THRESH_TOZERO_INV)#Cualquier pixel cuya intensidad no supere el umbral se establecerá a cero invertido
+
+cv2.imshow('Original', gray)#Muestra Imagen
+cv2.imwrite("Original.tif",gray)#Guarda Imagen
+cv2.imshow('Binario', dst)#Muestra Imagen
+cv2.imwrite("Binario.tif",dst)#Guarda Imagen
+cv2.imshow('Binario_Invertido', dsti)#Muestra Imagen Binario Invertido
+cv2.imwrite("Binario_Invertido.tif",dsti)#Guarda Imagen Binario Invertido
+cv2.imshow('Binario_DPixel', dti)#Muestra Imagen Binario Invertido
+cv2.imwrite("Binario_DPixel.tif",dti)#Guarda Imagen Binario Invertido
+cv2.imshow('Binario_Cero', dto)#Muestra Imagen Binario Invertido
+cv2.imwrite("Binario_Cero.tif",dto)#Guarda Imagen Binario Invertido
+cv2.imshow('Binario_CeroInvertido', dtoi)#Muestra Imagen Binario Invertido
+cv2.imwrite("Binario_CeroInvertido.tif",dtoi)#Guarda Imagen Binario Invertido
+
+
+cv2.waitKey(0)
